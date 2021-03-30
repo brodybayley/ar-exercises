24a3a0ad0067dd634745cd38e07d020ce97eefcf
Brody Bayley <brodybayley@gmail.com>
2021-03-30 06:25:20 +0000

    One to many association ex 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,19 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+#Add the following code directly inside the Store model (class)
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+#Add the following code directly inside the Employee model (class)
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+#Add some data into employees
+@store1.employees.create(first_name: "Brody", last_name: "Boo", hourly_rate: 100)
+@store1.employees.create(first_name: "Melanie", last_name: "Tooo", hourly_rate: 80)
+@store1.employees.create(first_name: "Tamara", last_name: "Chav", hourly_rate: 60)
+@store2.employees.create(first_name: "Shaungel", last_name: "Rob", hourly_rate: 90)
+@store2.employees.create(first_name: "Chris", last_name: "Nel", hourly_rate: 20)
+@store2.employees.create(first_name: "Peter", last_name: "Pan", hourly_rate: 50)
+@store1.employees.create(first_name: "Helen", last_name: "Yo", hourly_rate: 60)
